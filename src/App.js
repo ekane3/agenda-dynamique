5e579b1f0649b1d68695555498416b58f38871fa
@itishermann <hello@itishermann.me>
2021-03-31 20:32:05 +0200

    native base customization finished

--- src/App.js
+++ src/App.js
@@ -1,62 +1,18 @@
-import 'react-native-gesture-handler';
 import React from 'react';
-import {  StyleSheet,Text,Button,View, useWindowDimensions } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator,DrawerContentScrollView,DrawerItemList,DrawerItem }from '@react-navigation/drawer';
-import Home from './Screens/Home';
-import Search from './Screens/Search';
-import Contact from './Screens/Contact';
-import Settings from './Screens/Settings';
-import Maps from './Screens/Maps';
-import Quitter from './Screens/Quitter';
+import {Container, Content, Text, StyleProvider} from 'native-base';
+import getTheme from '../native-base-theme/components';
+import custom from '../native-base-theme/variables/custom';
 
-const Drawer = createDrawerNavigator();
-
-function CustomDrawerContent(props){
-  return(
-    <DrawerContentScrollView>
-      <DrawerItemList {...props}/>
-      <DrawerItem label="Quitter" onPress={() => alert('Quitter ?')}/>
-    </DrawerContentScrollView>
-  );
-}
-
-function App() {
-  //To have the users phone dimension for the drawer type
-  const dimensions = useWindowDimensions();
-
+const App = () => {
   return (
-    <NavigationContainer> 
-      <Drawer.Navigator 
-        drawerContent={props=> <CustomDrawerContent {...props} /> }
-        initialRouteName="Accueil"
-        drawerType={dimensions.width >= 768 ? 'permanent':'slide'}
-        drawerStyle={{backgroundColor:'#fffffa'}}
-      >
-        <Drawer.Screen name="Accueil" component={Home} />
-        <Drawer.Screen name="Rechercher" component={Search} />
-        <Drawer.Screen name="Contact" component={Contact} />
-        <Drawer.Screen name="Settings" component={Settings} />
-        <Drawer.Screen name="Carte" component={Maps} />
-        <Drawer.Screen name="Infos" component={Quitter} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <StyleProvider style={getTheme(custom)}>
+      <Container>
+        <Content>
+          <Text>I have changed the text color.</Text>
+        </Content>
+      </Container>
+    </StyleProvider>
   );
 };
-
-
-const styles = StyleSheet.create({
-  continer:{
-    flex: 1,
-    backgroundColor:"#fff",
-    alignItems:"center",
-    justifyContent:"center"
-  },
-  text: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  }
-});
 
 export default App;
