aab792765570a6770872797ede65a3f1b644d7ae
@itishermann <hello@itishermann.me>
2021-03-31 19:57:04 +0200

    renaming part 2

--- android/app/src/main/java/com/infolimo/agenda_dynamique/MainActivity.java
+++ android/app/src/main/java/com/infolimo/agenda_dynamique/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mobile;
+package com.infolimo.agenda_dynamique;
 
 import com.facebook.react.ReactActivity;
 
@@ -11,6 +11,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "mobile";
+    return "AgendaDynamique";
   }
 }
